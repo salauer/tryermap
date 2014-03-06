c622b7aad3eeaeecd15b5b37c478b28ad1d03215
Koalha <konsta.happonen@gmail.com>
2014-03-06 09:54:32 +0200

    working again

--- custom_map.R
+++ custom_map.R
@@ -7,21 +7,18 @@ geographyConfig = list(
 dataUrl="https://raw.github.com/Koalha/tryermap/master/kunnat2.topojson"
 ),
 
-  scope = "id",
-  setProjection = '#! function( element, options ) {
-
-  var projection, path;
-
-  projection = d3.transverseMercator()
-  .rotate([-27,-65,0])
-  .scale(element.offsetWidth)
-  .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+   scope = "id",
+setProjection = '#! function( element, options ) {
+   var projection, path;
+   projection = d3.geo.transverseMercator()
+    .rotate([-27,-65,0])
+    .scale(2000)
+    .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
 
    path = d3.geo.path().projection( projection );
    return {path: path, projection: projection};
-
   } !#'
-
 )
+
 
 maps
