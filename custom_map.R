0d46e81816b8d48fb1e53cc15b012a68a6dfac95
Koalha <konsta.happonen@gmail.com>
2014-03-06 09:28:14 +0200

    JEESSSa

---
+++ custom_map.R
@@ -0,0 +1,27 @@
+library(rMaps)
+
+maps = Datamaps$new()
+
+maps$set(
+geographyConfig = list(
+dataUrl="https://raw.github.com/Koalha/tryermap/master/kunnat2.topojson"
+),
+
+  scope = "id",
+  setProjection = '#! function( element, options ) {
+
+  var projection, path;
+
+  projection = d3.transverseMercator()
+  .rotate([-27,-65,0])
+  .scale(element.offsetWidth)
+  .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+
+   path = d3.geo.path().projection( projection );
+   return {path: path, projection: projection};
+
+  } !#'
+
+)
+
+maps
